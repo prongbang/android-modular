755b54c62bbc60fe7a317aa1e2b474f4af98b9bf
prongbang <prongbang@gmail.com>
2020-07-10 15:45:53 +0700

    Config Gradle Dependency Management

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,10 @@
+/**
+ * @ref https://caster.io/lessons/gradle-dependency-management-using-kotlin-and-buildsrc-for-buildgradle-autocomplete-in-android-studio
+ */
+plugins {
+	`kotlin-dsl`
+}
+
+repositories {
+	jcenter()
+}
